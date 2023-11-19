7b8ce478fee91dd6a1fa7c4e54d597498afd7a67
António Rocha <afgrocha98@gmail.com>
2023-11-19 12:41:31 +0000

    Create window

--- Main.cpp
+++ Main.cpp
@@ -2,7 +2,32 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
+int windowWidth = 800;
+int windowHeight = 800;
+
 int main()
 {
+	glfwInit();
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(windowWidth, windowHeight, "OpenGL Kart", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 	return 0;
 }
